19435c92e384026eca3b1c477855ad27438d8232
vradko <justslavic@gmail.com>
2024-03-05 21:51:03 +0400

    Update

---
+++ README.md
@@ -0,0 +1,11 @@
+# Based library
+
+This library is the collection of structs and functions useful to me.
+Ignore this library, if those function are not useful to you.
+
+**@todo:**
+1. `serializer` is the struct that would allow to push into memory_buffer objects, like integers, strings, other memory buffers, etc.;
+2. `deserializer` is the struct that would allow to sweep through memory_buffer object, while interpreting the memory in it as objects, like integers, strings, other memory buffers, etc.;
+3. Remove platform-dependent code from root of the library, it should only be accecable through `platform.hpp` and `os/platform_xxxx.cpp`;
+4. Ring buffer;
+5. Good enough, but simple hash table;
---
+++ ascii.h
@@ -0,0 +1,54 @@
+#ifndef BASED__ASCII_H
+#define BASED__ASCII_H
+
+
+#define ASCII_NULL                   0
+#define ASCII_START_OF_HEADING       1
+#define ASCII_START_OF_TEXT          2
+#define ASCII_END_OF_TEST            3
+#define ASCII_END_OF_TRANSMISSION    4
+#define ASCII_ENQUIRY                5
+#define ASCII_ACKNOWLEDGE            6
+#define ASCII_BELL                   7
+#define ASCII_BACKSPACE              8
+#define ASCII_HORIZONTAL_TAB         9
+#define ASCII_LINE_FEED             10
+#define ASCII_VERTICAL_TAB          11
+#define ASCII_FORM_FEED             12
+#define ASCII_CARRIAGE_RETURN       13
+#define ASCII_SHIFT_OUT             14
+#define ASCII_SHIFT_IN              15
+#define ASCII_DATA_LINK_ESCAPE      16
+#define ASCII_DEVICE_CONTROL_1      17
+#define ASCII_DEVICE_CONTROL_2      18
+#define ASCII_DEVICE_CONTROL_3      19
+#define ASCII_DEVICE_CONTROL_4      20
+#define ASCII_NEGATIVE_ACKNOWLEDGE  21
+#define ASCII_SYNCHRONOUS_IDLE      22
+#define ASCII_END_OF_TRANSFER_BLOCK 23
+#define ASCII_CANCEL                24
+#define ASCII_END_OF_MEDIUM         25
+#define ASCII_SUBSTITUTE            26
+#define ASCII_ESCAPE                27
+#define ASCII_FILE_SEPARATOR        28
+#define ASCII_GROUP_SEPARATOR       29
+#define ASCII_RECORD_SEPARATOR      30
+#define ASCII_UNIT_SEPARATOR        31
+#define ASCII_DEL                   127
+
+
+/*
+static char ascii_table[128] = {
+    0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
+   16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+  ' ', '!', '"', '#', '$', '%', '&','\'', '(', ')', '*', '+', ',', '-', '.', '/',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?',
+  '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+  'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[','\\', ']', '^', '_',
+  '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', 127,
+};
+*/
+
+
+#endif // BASED__ASCII_H
