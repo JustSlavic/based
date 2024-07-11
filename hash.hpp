0d39c30531ea2b557aac09f2008e79f40f25f80d
vradko <justslavic@gmail.com>
2024-07-12 03:04:27 +0400

    Add hash function for memory region

--- hash.hpp
+++ hash.hpp
@@ -14,5 +14,17 @@ FORCE_INLINE uint32 hash_djb2(char const *cstr)
     return h;
 }
 
+FORCE_INLINE uint32 hash_djb2(byte *data, usize size)
+{
+    // http://www.cse.yorku.ca/~oz/hash.html
+    uint32 h = 5381;
+    for (usize i = 0; i < size; i++)
+    {
+        int c = data[i];
+        h = ((h << 5) + h) + c; /* h * 33 + c */
+    }
+    return h;
+}
+
 
 #endif // BASED__HASH_HPP
